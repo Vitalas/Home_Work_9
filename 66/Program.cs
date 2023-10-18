c24d5d8a3fe70563ad2524c0ea5db2401a26f5a0
Vitalas <Vitalas_2004@mail.ru>
2023-10-18 21:54:28 +0300

    add 66_4

--- 66/Program.cs
+++ 66/Program.cs
@@ -3,17 +3,6 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8 -> 30
 
-Console.Write("Введитеи первое натуральное число (M): ");
-int numberM = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введитеи второе натуральное число (N): ");
-int numberN = Convert.ToInt32(Console.ReadLine());
-
-if (numberM < 1 || numberN < 1)
-{
-    Console.WriteLine("Некорректный ввод!");
-    return;
-}
-
 int SumDigit(int numM, int numN)
 {
     if (numM < numN)
@@ -27,6 +16,17 @@ int SumDigit(int numM, int numN)
         if (numM == numN) return numN;
         return numM + SumDigit(numM - 1, numN);
     }
+}
+
+Console.Write("Введитеи первое натуральное число (M): ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введитеи второе натуральное число (N): ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+if (numberM < 1 || numberN < 1)
+{
+    Console.WriteLine("Некорректный ввод!");
+    return;
 }
 
 int sum = SumDigit(numberM, numberN);
