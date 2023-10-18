74ba005ca755eaf58ba811bd3d611534e2cb6302
Vitalas <Vitalas_2004@mail.ru>
2023-10-18 22:27:15 +0300

    add 68

--- 68/Program.cs
+++ 68/Program.cs
@@ -2,3 +2,31 @@
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
 
+// функция ack(n, m)
+//    пока n ≠ 0
+//      если m = 0
+//        m:= 1
+//      иначе
+//        m:= ack(n, m - 1)
+//      n:= n - 1
+//    вернуть m + 1
+
+int Ack(int n, int m)
+{
+    while (n != 0)
+    {
+        if (m == 0) m = 1;
+        else
+        {
+            m = Ack(n, m - 1);
+        }
+        n--;
+    }
+    return m + 1;
+}
+
+int ack1 = Ack(4, 0);
+Console.WriteLine($"n = 4, m = 0 -> {ack1}");
+
+int ack2 = Ack(3, 11);
+Console.WriteLine($"n = 3, m = 11 -> {ack2}");
